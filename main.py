ae1312a0e7f433d9390f99c6c10c592946ca41c9
ikuma <chemicalbondo@gmail.com>
2024-06-07 16:59:38 +0900

    単一の対象で置換する処理を追加

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description='Update wikilinks in markdown files
 parser.add_argument('--all', '-a', action='store_true', help='Update all wikilinks in all files')
 parser.add_argument('--specific', '-s', action='store_true',
                     help='Update wikilinks in all files with a specific wiki file.')
+parser.add_argument('--one', '-o', action='store_true', help='Update one wikilinks in one file')
 
 
 def all_update():
@@ -31,12 +32,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     load_dotenv()
     base_dir = Path(os.getenv("BASE_PATH"))
-    files = [p for p in base_dir.rglob('*.md')]
+    if not args.one:
+        files = [p for p in base_dir.rglob('*.md')]
 
     if args.all:
         all_update()
     elif args.specific:
-        target_wiki_file = input('Please specify the target wiki file: ')
+        target_wiki_file = input('Please specify the target wiki file path: ')
         specific_update(target_wiki_file)
+    elif args.one:
+        target_file = input('Please enter the file path to be replaced.')
+        target_wiki_file = input('Please enter the file path of the Wiki link update source.: ')
+        target_file_path = base_dir / target_file
+        target_wiki_file_path = base_dir / target_wiki_file
+        update_wikilinks(WikiSource(target_wiki_file_path), target_file_path)
     else:
-        print('Please specify the option with --all or --specific')
+        print('Please specify the option with --all or --specific or --one.')
